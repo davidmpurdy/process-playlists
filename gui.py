cea012f91e3d9be2791007e5d610e8fcdff7d842
David Purdy <davidpurdy@gmail.com>
2021-04-19 14:27:07 -0600

    Update interface to allow selecting multiple EVT source files at once

--- gui.py
+++ gui.py
@@ -15,13 +15,13 @@ settings = EasySettings("process_log.conf")
 
 top = tkinter.Tk()
 top.title ("Process a log file")
-file_name = None
+file_names = []
 
 ###
 # Load a file and save the path for future use (to open the dialog in the same place next time)
 ###
 def saveCallback():
-    global file_name
+    global file_names
 
     # use stored initial path if set 
     if settings.has_option('initialdir') and settings.get("initialdir") is not None:
@@ -30,39 +30,49 @@ def saveCallback():
         initialdir = "/"
 
     # get the filename from a file picking dialog
-    file_name =  filedialog.askopenfilename(initialdir = initialdir, title = "Select file")
+    file_name =  filedialog.askopenfilenames(initialdir = initialdir, title = "Select file")
+    
 
     if file_name is not None and len(file_name) > 0:
-        file_name_label.set(file_name)
+        file_names = list(top.tk.splitlist(file_name))
+        
+        file_name_label.set("\n".join(file_names))
 
-    # save the path for next time
-    path = os.path.realpath(file_name)
-    if path is not None:
-        settings.set("initialdir", path)
-        settings.save()
+    for name in file_names:
+        # save the path for next time
+        path = os.path.realpath(name)
+        if path is not None:
+            settings.set("initialdir", path)
+            settings.save()
 
 
 ###
 # Process the file and save it out 
 ###
 def procCallback():
-    global file_name
-    if file_name is not None and len(file_name) > 0:
-        process_file (file_name)
+    global file_names
+    if file_names is not None and len(file_names) > 0:
+        for name in list(file_names):
+            process_file (name)
+            file_names.remove(name)
+            file_name_label.set("\n".join(file_names))
+
+
+
 
 # file field label
 file_name_label = tkinter.StringVar()
 file_label = tkinter.Label(top, textvariable=file_name_label, width=50, justify="left", relief="groove")
 
 # buttons
-b1 = tkinter.Button(top, text ="Choose file", command = saveCallback)
+b1 = tkinter.Button(top, text ="Choose files", command = saveCallback)
 b2 = tkinter.Button(top, text ="Process", command = procCallback)
 
 # instructions
 instruction_label = tkinter.Label(top, justify="left", text="1) Select a file \n2) Click 'Process' to save the processed file as a CSV in the same location/same name as the original (but with a .csv extension)")
 
 # pack UI elements
-instruction_label.pack(side=tkinter.BOTTOM, pady=5, padx=3)
+instruction_label.pack(side=tkinter.TOP, pady=5, padx=3)
 file_label.pack(side=tkinter.LEFT, pady=3, padx=3)
 b2.pack(side=tkinter.RIGHT, pady=3, padx=3)
 b1.pack(side=tkinter.RIGHT, pady=3, padx=3)
